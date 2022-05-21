a7a9433103aa1c7f5ff555d3b1a45e2d146f5fe9
Whale Well Well <AsherEverettDev@gmail.com>
2022-05-21 16:54:18 -0400

    Added main.py

---
+++ src/main.py
@@ -0,0 +1,136 @@
+# TIC-TAC-TOE by @WhaleWellWell on GitHub
+
+X = "x"
+O = "O"
+LEGAL_MOVES = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+SPACE = "."
+BOARD_TOP    = [ "{}".format(SPACE), "{}".format(SPACE), "{}".format(SPACE) ]
+BOARD_MIDDLE = [ "{}".format(SPACE), "{}".format(SPACE), "{}".format(SPACE) ]
+BOARD_BOTTOM = [ "{}".format(SPACE), "{}".format(SPACE), "{}".format(SPACE) ]
+
+def displayBoard():
+  print("\n")
+  print("  ABC")
+  print("1 " + BOARD_TOP[0] + BOARD_TOP[1] + BOARD_TOP[2])
+  print("2 " + BOARD_MIDDLE[0] + BOARD_MIDDLE[1] + BOARD_MIDDLE[2])
+  print("3 " + BOARD_BOTTOM[0] + BOARD_BOTTOM[1] + BOARD_BOTTOM[2])
+  print("\n")
+
+def askForMove(player):
+  while True:
+    print("Please enter move, player " + player + "")
+    move = input("> ")
+
+    if move in LEGAL_MOVES:
+
+      # A
+      if move == "A1":
+        if BOARD_TOP[0] == SPACE:
+          BOARD_TOP[0] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "A2":
+        if BOARD_MIDDLE[0] == SPACE:
+          BOARD_MIDDLE[0] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "A3":
+        if BOARD_BOTTOM[0] == SPACE:
+          BOARD_BOTTOM[0] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      # B
+      if move == "B1":
+        if BOARD_TOP[1] == SPACE:
+          BOARD_TOP[1] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "B2":
+        if BOARD_MIDDLE[1] == SPACE:
+          BOARD_MIDDLE[1] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "B3":
+        if BOARD_BOTTOM[1] == SPACE:
+          BOARD_BOTTOM[1] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      # C
+      if move == "C1":
+        if BOARD_TOP[2] == SPACE:
+          BOARD_TOP[2] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "C2":
+        if BOARD_MIDDLE[2] == SPACE:
+          BOARD_MIDDLE[2] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      if move == "C3":
+        if BOARD_BOTTOM[2] == SPACE:
+          BOARD_BOTTOM[2] = player
+        else:
+          print("Not a legal move :{\n")
+          continue
+
+      break
+
+    else:
+      print("Not a legal move :{\n")
+      continue
+
+# Main Loop:
+def main():
+  turn = O
+  while True:
+    if turn == X:
+      turn = O
+    else:
+      turn = X
+
+    displayBoard()
+    askForMove(turn)
+
+    if BOARD_TOP[0] == turn and BOARD_TOP[1] == turn and BOARD_TOP[2] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_MIDDLE[0] == turn and BOARD_MIDDLE[1] == turn and BOARD_MIDDLE[2] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_BOTTOM[0] == turn and BOARD_BOTTOM[1] == turn and BOARD_BOTTOM[2] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_TOP[0] == turn and BOARD_MIDDLE[1] == turn and BOARD_BOTTOM[2] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_TOP[2] == turn and BOARD_MIDDLE[1] == turn and BOARD_BOTTOM[0] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_TOP[0] == turn and BOARD_MIDDLE[0] == turn and BOARD_BOTTOM[0] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_TOP[1] == turn and BOARD_MIDDLE[1] == turn and BOARD_BOTTOM[1] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+    if BOARD_TOP[2] == turn and BOARD_MIDDLE[2] == turn and BOARD_BOTTOM[2] == turn:
+      print("\n\nPlayer " + turn + " has won!!")
+      break
+
+if __name__ == '__main__':
+  main()
